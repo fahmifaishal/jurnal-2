2c264972b37aab0f49e9a248e8f5362014ac95bc
fahmifaishal <negarfahmi3@gmail.com>
2024-09-20 07:28:30 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("nama : negar fahmi faishal ahmad");
+        System.out.println("nim : 607062400111");
+        System.out.println("kelas : D3IF - 48 - 01");
+    }
+}
